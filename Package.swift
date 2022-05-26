deffc9f6cb2c6241b9c1e2e58844a3330180d4b1
Sreekanth <sreekanthreddy@Sreekanths-MacBook-Air.local>
2022-05-26 10:12:37 +0530

    Updated

--- Package.swift
+++ Package.swift
@@ -12,8 +12,10 @@ let package = Package(
             targets: ["RepoB"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/Sreekanth-Reddy-GB/RepoA.git",
+            .branch("main")
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
